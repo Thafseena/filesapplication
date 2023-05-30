8675c94cd1dd3e68403ff8c33d18d45ed7b620a2
Thafseena <thafseenamkd121996@gmail.com>
2023-05-30 14:20:43 +0530

    firebase upload files successfully

--- lib/app/modules/home/views/folder_view.dart
+++ lib/app/modules/home/views/folder_view.dart
@@ -1,21 +1,124 @@
-import 'package:filesapplication/app/modules/home/controllers/folder_controller.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:get/get.dart';
+// import 'package:filesapplication/app/modules/home/controllers/folder_controller.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter/src/widgets/framework.dart';
+// import 'package:get/get.dart';
 
-class FolderView extends GetView<FolderController>{
+// class FolderView extends GetView<FolderController>{
+//   @override
+//   Widget build(BuildContext context) {
+//     // TODO: implement build
+//      return SafeArea(
+//       child: SingleChildScrollView(
+//         child: Container(
+//           height: 400,
+//           width: 350,
+//         color: Colors.amberAccent,
+      
+//            ),
+//       ));
+//   }
+  
+// }
+
+import 'dart:io';
+
+import 'package:file_picker/file_picker.dart';
+import 'package:filesapplication/app/modules/home/api/firebase_api.dart';
+import 'package:filesapplication/app/modules/home/views/button_widget.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/material.dart';
+import 'package:path/path.dart';
+
+class FolderView extends StatefulWidget {
+  @override
+  _FolderViewState createState() => _FolderViewState();
+}
+
+class _FolderViewState extends State<FolderView> {
+  UploadTask? task;
+  File? file;
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-     return SafeArea(
-      child: SingleChildScrollView(
-        child: Container(
-          height: 400,
-          width: 350,
-        color: Colors.amberAccent,
-      
-           ),
-      ));
+    final fileName = file != null ? basename(file!.path) : 'No File Selected';
+
+    return Scaffold(
+      appBar: AppBar(
+        // title: Text(MyApp.title),
+        centerTitle: true,
+      ),
+      body: Container(
+        padding: EdgeInsets.all(32),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ButtonWidget(
+                text: 'Select File',
+                icon: Icons.attach_file,
+                onClicked: selectFile,
+              ),
+              SizedBox(height: 8),
+              Text(
+                fileName,
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+              ),
+              SizedBox(height: 48),
+              ButtonWidget(
+                text: 'Upload File',
+                icon: Icons.cloud_upload_outlined,
+                onClicked: uploadFile,
+              ),
+              SizedBox(height: 20),
+              task != null ? buildUploadStatus(task!) : Container(),
+            ],
+          ),
+        ),
+      ),
+    );
   }
-  
+
+  Future selectFile() async {
+    final result = await FilePicker.platform.pickFiles(allowMultiple: false);
+
+    if (result == null) return;
+    final path = result.files.single.path!;
+
+    setState(() => file = File(path));
+  }
+
+  Future uploadFile() async {
+    if (file == null) return;
+
+    final fileName = basename(file!.path);
+    final destination = 'files/$fileName';
+
+    task = FirebaseApi.uploadFile(destination, file!);
+    setState(() {});
+
+    if (task == null) return;
+
+    final snapshot = await task!.whenComplete(() {});
+    final urlDownload = await snapshot.ref.getDownloadURL();
+
+    print('Download-Link: $urlDownload');
+  }
+
+  Widget buildUploadStatus(UploadTask task) => StreamBuilder<TaskSnapshot>(
+        stream: task.snapshotEvents,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            final snap = snapshot.data!;
+            final progress = snap.bytesTransferred / snap.totalBytes;
+            final percentage = (progress * 100).toStringAsFixed(2);
+
+            return Text(
+              '$percentage %',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            );
+          } else {
+            return Container();
+          }
+        },
+      );
 }
