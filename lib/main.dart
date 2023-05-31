a52cf96e05dbcc7d04b6bab90279e74976cc6997
Thafseena <thafseenamkd121996@gmail.com>
2023-05-31 17:49:17 +0530

    it completed firebasenotification

--- lib/main.dart
+++ lib/main.dart
@@ -59,6 +59,7 @@ import 'package:filesapplication/app/modules/home/bindings/home_binding.dart';
 import 'package:filesapplication/app/modules/home/views/te.dart';
 import 'package:filesapplication/app/routes/app_pages.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -79,6 +80,22 @@ import 'package:get/get.dart';
 //       );
 // }
 
+const AndroidNotificationChannel channel = AndroidNotificationChannel(
+    'high_importance_channel', // id
+    'High Importance Notifications', // title
+    description:
+        'This channel is used for important notifications.', // description
+    importance: Importance.high,
+    playSound: true);
+
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
+
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp();
+  print('A bg message just showed up :  ${message.messageId}');
+}
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await SystemChrome.setPreferredOrientations([
@@ -86,10 +103,24 @@ void main() async {
     DeviceOrientation.portraitDown,
   ]);
   await Firebase.initializeApp(
-    // options: DefaultFirebaseOptions.currentPlatform,
+    // options: DefaultFirebaseOptions.currentPlatform,'
+
   );
-  // final fcmtoken=await FirebaseMessaging.instance.getToken();
-  // print(fcmtoken);
+      final fcmtoken=await FirebaseMessaging.instance.getToken();
+  print(fcmtoken);
+  
+  
+   FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+     await flutterLocalNotificationsPlugin
+      .resolvePlatformSpecificImplementation<AndroidFlutterLocalNotificationsPlugin>()
+      ?.createNotificationChannel(channel);
+
+  await FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(
+    alert: true,
+    badge: true,
+    sound: true,
+  );
+  
   HomeBinding().dependencies();
   runApp(
     GetMaterialApp(
