dbed7b482eda1bf64838d21b9808951195f0bc76
Thafseena <thafseenamkd121996@gmail.com>
2023-05-31 17:48:36 +0530

    firebase push notification tested for creation and successed

--- lib/main.dart
+++ lib/main.dart
@@ -57,61 +57,65 @@
 
 import 'package:filesapplication/app/modules/home/bindings/home_binding.dart';
 import 'package:filesapplication/app/modules/home/views/te.dart';
-
 import 'package:filesapplication/app/routes/app_pages.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 
-void main(){
-    runApp(MyApp());
-}
-class MyApp extends StatelessWidget {
-  static final String title = 'Firebase Upload';
+// void main(){
+//     runApp(MyApp());
+// }
+// class MyApp extends StatelessWidget {
+//   static final String title = 'Firebase Upload';
 
-  @override
-  Widget build(BuildContext context) => MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: title,
-        theme: ThemeData(primarySwatch: Colors.green),
-        home: CreateTest(),
-      );
-}
+//   @override
+//   Widget build(BuildContext context) => MaterialApp(
+//         debugShowCheckedModeBanner: false,
+//         title: title,
+//         theme: ThemeData(primarySwatch: Colors.green),
+//         home: CreateTest(),
+//       );
+// }
 
-// /* void main() async {
-//   WidgetsFlutterBinding.ensureInitialized();
-//   await SystemChrome.setPreferredOrientations([
-//     DeviceOrientation.portraitUp,
-//     DeviceOrientation.portraitDown,
-//   ]);
-//   await Firebase.initializeApp();
-//   HomeBinding().dependencies();
-//   runApp(
-//     GetMaterialApp(
-//       title: "Application",
-//        initialRoute: AppPages.INITIAL,
-//     //  home: CreateTest(),
-//       getPages: AppPages.routes,
-//       debugShowCheckedModeBanner: false,
-//       themeMode: ThemeMode.dark,
-//       darkTheme: ThemeData.dark().copyWith(
-//         hintColor: Colors.yellow,
-//         primaryColor: Color(0xff141A31),
-//         primaryColorDark: Color(0xff081029),
-//         scaffoldBackgroundColor: Color(0xff141A31),
-//         textSelectionTheme: TextSelectionThemeData(cursorColor: Colors.yellow),
-//         elevatedButtonTheme: ElevatedButtonThemeData(
-//           style: ButtonStyle(
-//               shape: MaterialStateProperty.all(
-//                 RoundedRectangleBorder(
-//                   borderRadius: BorderRadius.circular(20),
-//                 ),
-//               ),
-//               backgroundColor: MaterialStateProperty.all(Colors.yellowAccent),
-//               padding: MaterialStateProperty.all(EdgeInsets.all(14))),
-//         ),
-//       ),
-//     ),
-//   );
-// }*/
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+  await Firebase.initializeApp(
+    // options: DefaultFirebaseOptions.currentPlatform,
+  );
+  // final fcmtoken=await FirebaseMessaging.instance.getToken();
+  // print(fcmtoken);
+  HomeBinding().dependencies();
+  runApp(
+    GetMaterialApp(
+      title: "Application",
+       initialRoute: AppPages.INITIAL,
+    //  home: CreateTest(),
+      getPages: AppPages.routes,
+      debugShowCheckedModeBanner: false,
+      themeMode: ThemeMode.dark,
+      darkTheme: ThemeData.dark().copyWith(
+        hintColor: Colors.yellow,
+        primaryColor: Color(0xff141A31),
+        primaryColorDark: Color(0xff081029),
+        scaffoldBackgroundColor: Color(0xff141A31),
+        textSelectionTheme: TextSelectionThemeData(cursorColor: Colors.yellow),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ButtonStyle(
+              shape: MaterialStateProperty.all(
+                RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+              ),
+              backgroundColor: MaterialStateProperty.all(Colors.yellowAccent),
+              padding: MaterialStateProperty.all(EdgeInsets.all(14))),
+        ),
+      ),
+    ),
+  );
+}
